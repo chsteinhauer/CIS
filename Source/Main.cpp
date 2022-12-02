4457514f4ee904be4d93776f4b8aa0ba9eec3f15
Tobias <tobias.l.sonderbo@gmail.com>
2022-12-02 10:59:27 +0100

    Add gammatone submodule

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,10 +6,6 @@
   ==============================================================================
 */
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
-
 #include <JuceHeader.h>
 #include "MainComponent.h"
 
