ed6b022ca71e955ca0e9621edf3f7891f8d76847
Christina Steinhauer <christina.spost@gmail.com>
2022-12-07 11:23:30 +0100

    Create module

---
+++ Source/cis_module.h
@@ -0,0 +1,50 @@
+/*
+  ==============================================================================
+   This file is part of the JUCE library.
+   Copyright (c) 2022 - Raw Material Software Limited
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
+   By using JUCE, you agree to the terms of both the JUCE 7 End-User License
+   Agreement and JUCE Privacy Policy.
+   End User License Agreement: www.juce.com/juce-7-licence
+   Privacy Policy: www.juce.com/juce-privacy-policy
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
+   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+  ==============================================================================
+*/
+
+
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.md file.
+ BEGIN_JUCE_MODULE_DECLARATION
+  ID:                 CIS
+  vendor:             juce
+  version:            1.0.0
+  name:               Cochlear Implant Simulation
+  description:        
+  website:            
+  license:            
+  minimumCppStandard: 17
+  dependencies:       
+ END_JUCE_MODULE_DECLARATION
+*******************************************************************************/
+
+
+#pragma once
+
+
+//==============================================================================
+namespace cis
+{
+    
+    class MainComponent;
+}
+
+#include "MainComponent.h"
+#include "MainComponent.css"
